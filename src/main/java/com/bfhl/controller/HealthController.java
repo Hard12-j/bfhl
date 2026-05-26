2e6810497de7cba35a2cede25afaadac1a6e052c
unknown <joshi12hardik@gmail.com>
2026-05-26 15:39:11 +0530

    Add GET /health endpoint

---
+++ src/main/java/com/bfhl/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.bfhl.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        return ResponseEntity.ok(Map.of("status", "UP"));
+    }
+}
