fc7c35a8dbf8e03e1655a6194171ad557b7b7ea6
unknown <joshi12hardik@gmail.com>
2026-05-26 16:13:40 +0530

    output

--- src/main/java/com/bfhl/dto/BfhlResponse.java
+++ src/main/java/com/bfhl/dto/BfhlResponse.java
@@ -31,8 +31,8 @@ public class BfhlResponse {
     @JsonProperty("special_characters")
     private List<String> specialCharacters;
 
-    @JsonProperty("sepcial_characters")
-    private List<String> sepcialCharacters;
+    // @JsonProperty("sepcial_characters")
+    // private List<String> sepcialCharacters;
 
     private String sum;
 
